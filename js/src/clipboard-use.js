87293bf94a06b926a7412dce3653a1e051bbe9e1
System Administrator <root@kevindeMacBook-Air.local>
2020-11-06 22:53:21 +0800

    Site updated: 2020-11-06 22:53:21

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -4,7 +4,7 @@
   var initCopyCode = function(){
     var copyHtml = '';
     copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-    copyHtml += '  <i class="fa fa-globe"></i><span>copycode</span>';
+    copyHtml += '  <i class="fa fa-globe"></i><span>复制</span>';
     copyHtml += '</button>';
     $(".highlight .code pre").before(copyHtml);
     new ClipboardJS('.btn-copy', {
